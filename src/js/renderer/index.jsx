ebb56160089941eed3c33bcd8f27998d239c760c
yshrsmz <the.phantom.bane@gmail.com>
2015-05-17 01:27:53 +0900

    :heavy_plus_sign: material-ui

--- src/js/renderer/index.jsx
+++ src/js/renderer/index.jsx
@@ -1,14 +1,21 @@
+'use strict';
+
 import React from 'react';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import mui from 'material-ui';
+
+window.React = React;
+injectTapEventPlugin();
 
 let Content = React.createClass({
     render() {
-    return (
-        <div><h1>This is React generated contents.</h1></div>
-);
-}
+        return (
+            <div><h1>This is React generated contents.</h1></div>
+        );
+    }
 });
 
 React.render(
-<Content/>,
+    <Content/>,
     document.getElementById('content')
 );
